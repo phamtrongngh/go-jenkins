fb62527fe10dd13f32f21a426520a76bfbf237f7
phamtrongngh <benalpha1105@gmail.com>
2022-06-05 12:17:54 +0700

    s

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	http.ListenAndServe(":9999", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.ListenAndServe(":9998", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	}))
 }
